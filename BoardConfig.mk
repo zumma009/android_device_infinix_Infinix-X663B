a8f55c9aeb63aebd87f9c872dc58069b933f3e15
zumma009 <152015264+zumma009@users.noreply.github.com>
2023-11-29 12:46:52 +0700

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -92,6 +92,9 @@ TARGET_BOARD_PLATFORM := mt6768
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# LZMA compression for recovery's & kernel ramdisk....
+LZMA_RAMDISK_TARGETS := boot
+
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
